9f4d3cedc0ba48ecaf8b33b547b42f44c8362b01
Hudson Eboso <ebosoh@gmail.com>
2023-02-28 09:30:04 +0300

    Remove the API key

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 <?php
   $captcha = $_POST['g-recaptcha-response'];
-  $secret_key = '6Ldez7gkAAAAAIQyqM3V53IpAiBajrcRgfHDX3C_';
+  $secret_key = '';
   $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret_key."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
   $response = json_decode($response, true);
 
